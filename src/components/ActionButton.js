721d11eac8ee063995b487233dc2c0a13e78772c
Rishabh Kumar Soni <rishabh.soni@orchids.edu.in>
2022-01-15 00:43:52 +0530

    deleted an unused component

--- src/components/ActionButton.js
+++
@@ -1,9 +0,0 @@
-const ActionButton = (props) => {
-	return (
-		<button className="action" onClick={() => props.action()}>
-			{props.actionName}
-		</button>
-	);
-};
-
-export default ActionButton;
